f80d8670e675f3c5133ede0c6172e3ab575dc16e
jesushilden <peter.e.hilden@gmail.com>
2019-01-24 16:13:50 +0200

    validation for posting

--- controllers/highscores.js
+++ controllers/highscores.js
@@ -12,16 +12,40 @@ highscoreRouter.post('/', async (request, response) => {
     try {
         const body = request.body
 
-        if (body.user === undefined || body.token === undefined || body.installationId === undefined || body.score === undefined) {
-            return response.status(400).json({ error: 'some fields missing' })
-        }
-
         const highscore = new Highscore({
             user: body.user,
             token: body.token,
             installationId: body.installationId,
             score: body.score
         })
+
+        const validationError = highscore.validateSync()
+        const users = await Highscore.countDocuments({ user: highscore.user })
+        const tokens = await Highscore.countDocuments({ token: highscore.token })
+        const installationIds = await Highscore.countDocuments({ installationId: highscore.installationId })
+
+        if (validationError || users || tokens || installationIds) {
+            const errorMessages = []
+            if (users) errorMessages.push('Username already taken')
+            if (tokens) errorMessages.push('Token already taken')
+            if (installationIds) errorMessages.push('InstallationId already taken')
+
+            if (validationError) {
+                if (validationError.errors['user']) {
+                    errorMessages.push(validationError.errors['user'].message)
+                }
+                if (validationError.errors['token']) {
+                    errorMessages.push(validationError.errors['token'].message)
+                }
+                if (validationError.errors['installationId']) {
+                    errorMessages.push(validationError.errors['installationId'].message)
+                }
+                if (validationError.errors['score']) {
+                    errorMessages.push(validationError.errors['score'].message)
+                }
+            }
+            return response.status(400).json({ error: errorMessages })
+        }
 
         const savedHighscore = await highscore.save()
 
